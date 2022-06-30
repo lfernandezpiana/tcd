e88e8fc134c15c0fba7a59662d57f6a8ebcf948a
lucas <lucasfernandezpiana@gmail.com>
2022-06-30 17:52:42 -0300

    cambios para poder usar la distancia harvesing en las coordenadas polares

--- R/car2pol.R
+++ R/car2pol.R
@@ -1,13 +1,53 @@
-#### CONVERT CARTESIAN COORDINATES TO POLAR COORDINATES
+#### CONVERT CARTESIAN OR GEOGRAPHICAL COORDINATES TO POLAR COORDINATES
 
 
-cart2pol = function(trayectory)
-  # Trayectory: n x 2 matrix. Each column has a coordinate.
-  # Returns: n x 2 matrix. The first column is radius and the second is angle.
-{
-  x = as.numeric(trayectory[,1]) - trayectory[1,1] ; y = as.numeric(trayectory[,2]) - trayectory[1,2]
-  radius = sqrt(x^2 + y^2)
-  gamma = atan2(y,x)
+# Calculates harvesing distance between points in km
+harvesing_dist = function(lon1, lat1, lon2, lat2) {
+  # distance between latitudes and longitudes
+  dlat = (lat2 - lat1)*pi/180
+  dlon = (lon2 - lon1)*pi/180
 
-  return(cbind(radius,gamma))
+  # convert to radians
+  lat1 = (lat1)*pi/180
+  lat2 = (lat2)*pi/180
+
+  # apply formulae
+  a = sin(dlat/2)^2 + (sin(dlon/2)^2)*cos(lat1)*cos(lat2)
+  rad = 6371
+  c = 2*sin(sqrt(a))
+
+  return(rad*c)
 }
+
+
+# Convert to polar coordinates
+polar_coordinates = function(trajectory, type="cartesian") {
+  # Trajectory: n x 2 matrix. Each column has a coordinate.
+  # Type: character. If "cartesian" trajectories are in cartesian coordinates;
+  #       if "geografical" coordiantes are in long/lat format.
+  # Returns: n x 2 matrix. The first column is radius and the second is angle.
+
+  if (type=="cartesian") {
+    x = as.numeric(trajectory[,1]) - trajectory[1,1] ; y = as.numeric(trajectory[,2]) - trajectory[1,2] # move to (0,0)
+    radius = sqrt(x^2 + y^2)
+    gamma = atan2(y,x)
+    return(cbind(radius,gamma))
+  }
+
+  else if (type=="geographical") {
+    n = nrow(trajectory)
+    x = as.numeric(trajectory[,1]) - trajectory[1,1] ; y = as.numeric(trajectory[,2]) - trajectory[1,2] # move to (0,0)
+    gamma = atan2(y,x)
+    radius = numeric(n)
+    for (i in 1:n) radius[i] = harvesing_dist(0,0,x[i],y[i])
+    return(cbind(radius,gamma))
+  }
+
+  else {
+    print("Choose type as geographical or cartesian")
+  }
+
+}
+
+
+
